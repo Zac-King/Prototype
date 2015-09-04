baf1d30df0e3de7b72fd7bd58a4cfde741873872
Eric <EricMouledoux@Gmail.com>
2015-09-04 12:50:27 -0500

    Card spacing

---
+++ Prototype/Assets/Scripts/CardSpaceing.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CardSpaceing : MonoBehaviour
+{
+    public float m_maxCards;
+
+    public Vector3 m_min;
+    public Vector3 m_max;
+
+    public List<GameObject> cards;
+
+
+    public void SpaceCards(List<GameObject> a_cards)
+    {
+        for (int i = 0; i < a_cards.Count; ++i)
+        {
+            a_cards[i].transform.position = m_min + (m_max * (i / (m_maxCards / 2)));
+        }
+    }
+
+    void GetChildren()
+    {
+        cards.Clear();
+
+        foreach (Transform c in transform)
+        {
+            cards.Add(c.gameObject);
+        }
+    }
+    void Start()
+    {
+        m_min  = (transform.position - transform.right) / 2 * transform.localScale.x;
+        m_max  = (transform.position + transform.right) / 2 * transform.localScale.x;
+    }
+
+    void Update()
+    {
+        GetChildren();
+        SpaceCards(cards);
+    }
+
+}
