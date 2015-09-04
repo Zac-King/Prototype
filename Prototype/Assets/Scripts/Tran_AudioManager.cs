2d56251dbc92d0acebb740040771e3d01b7bf6e1
coringuyen <coringuyen96@gmail.com>
2015-09-04 15:01:37 -0500

    Audio Manager
    3 audio so far

---
+++ Prototype/Assets/Scripts/Tran_AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tran_AudioManager : MonoBehaviour {
+
+    public AudioSource manager;
+    public AudioClip start_audioclip;
+    public AudioClip exit_audioclip;
+    public AudioClip shuffle_audioclip;
+    public AudioClip mouseOnCard_audioclip;
+    public AudioClip clickOnCard_audioclip;
+    public AudioClip cardPlay_audioclip;
+    
+    void play_the_music(AudioClip music)
+    {
+        manager.loop = false;
+        manager.clip = music;
+        manager.Play();
+        
+    }
+    
+    public void Start_Audio()
+    {
+        play_the_music(start_audioclip);
+    }
+    public void Exit_Audio()
+    {
+        play_the_music(exit_audioclip);
+    }
+
+    public void Shuffle_Audio()
+    {
+        play_the_music(shuffle_audioclip);
+    }
+
+    public void MouseOnCard_Audio()
+    {
+        play_the_music(mouseOnCard_audioclip);
+    }
+
+    public void ClickOnCard_Audio()
+    {
+        play_the_music(clickOnCard_audioclip);
+    }
+
+    public void CardPlay_Audio()
+    {
+        play_the_music(cardPlay_audioclip);
+    }
+}
---
+++ Prototype/Assets/Scripts/Tran_testAudio.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tran_testAudio : MonoBehaviour {
+
+	void Update ()
+    {
+       
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Tran_AudioManager>().Start_Audio();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GetComponent<Tran_AudioManager>().Exit_Audio();
+        }
+    }
+}
